973065c2d4170ff8e98240bebe9d3868a909c327
Gonzalo Albornoz <galbornoz@frba.utn.edu.ar>
2023-04-16 19:49:46 -0300

    modified

---
+++ 00-CHelloWorld/README.md
@@ -0,0 +1,7 @@
+# TP0 Hello World!
+
+• Curso: K2006
+• Año de cursada: 2023
+• Legajo: 20231693
+• Apellido: Albornoz
+• Nombre: Gonzalo Ignacio
---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+
+int main(void){
+
+
+    printf("Hello World! \n");
+
+   FILE* HelloWorld = fopen("output.txt", "wt");
+
+
+    printf (HelloWorld, "Hello World");
+
+}
